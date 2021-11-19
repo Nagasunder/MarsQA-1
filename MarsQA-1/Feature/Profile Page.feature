74e895d33154efac9217f48bc66aff92c0721e79
sunda <sunda@DESKTOP-BPNB4T5>
2021-11-19 14:47:43 +0000

    Added new feature file Profile page

---
+++ MarsQA-1/Feature/Profile Page.feature
@@ -0,0 +1,8 @@
+﻿Feature: Profile Page
+	Simple calculator for adding two numbers
+
+@mytag
+Scenario: Adding Availability, Hours and Target earn to profile page
+Given Add availability to profile page
+And Add hours to profile page
+Then Add target earn to profile page
---
+++ MarsQA-1/Feature/ProfilePage.cs
@@ -0,0 +1,36 @@
+﻿using MarsQA_1.Helpers;
+using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+
+namespace MarsQA_1.Feature
+{
+    [Binding]
+    public class ProfilePage
+    {
+
+        // Add availablity to progile page
+        [Given(@"Add availability to profile page")]
+        public void GivenAddAvailabilityToProfilePage()
+        {
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated outline small write icon']")).Click();
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated content']/span/select/option[2]")).Click();
+        }
+        
+        // Add hours to profile page
+        [Given(@"Add hours to profile page")]
+        public void GivenAddHoursToProfilePage()
+        {
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated outline small write icon']")).Click();
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated content']/span/select/option[2]")).Click();
+        }
+        
+        // Add earn targets to progile page
+        [Then(@"Add target earn to profile page")]
+        public void ThenAddTargetEarnToProfilePage()
+        {
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated outline small write icon']")).Click();
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated content']/span/select/option[2]")).Click();
+        }
+    }
+}
