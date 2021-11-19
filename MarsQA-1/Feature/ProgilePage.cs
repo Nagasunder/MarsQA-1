290ce588cacf8a28e5cd205affd958d5f935b71d
sunda <sunda@DESKTOP-BPNB4T5>
2021-11-19 15:35:03 +0000

    Added new feature by name profile page

---
+++ MarsQA-1/Feature/ProgilePage.cs
@@ -0,0 +1,32 @@
+﻿using MarsQA_1.Helpers;
+using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+
+namespace MarsQA_1.Feature
+{
+    [Binding]
+    public class ProgilePage
+    {
+        [Given(@"Add avaiability to the profile page")]
+        public void GivenAddAvaiabilityToTheProfilePage()
+        {
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated outline small write icon']")).Click();
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated content']/span/select/option[2]")).Click();
+        }
+        
+        [Given(@"Add number of hours you can wook a week")]
+        public void GivenAddNumberOfHoursYouCanWookAWeek()
+        {
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated outline small write icon']")).Click();
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated content']/span/select/option[2]")).Click();
+        }
+        
+        [Then(@"Add the taget earned in a month")]
+        public void ThenAddTheTagetEarnedInAMonth()
+        {
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated outline small write icon']")).Click();
+            Driver.driver.FindElement(By.XPath("//*[@class='right floated content']/span/select/option[2]")).Click();
+        }
+    }
+}
