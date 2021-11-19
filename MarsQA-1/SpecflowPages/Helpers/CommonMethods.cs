0e31f5627c92cd2e076dc95bc04063f44b96f497
sunda <sunda@DESKTOP-BPNB4T5>
2021-11-19 14:31:16 +0000

    comment on the code for screenshots and reports removed

--- MarsQA-1/SpecflowPages/Helpers/CommonMethods.cs
+++ MarsQA-1/SpecflowPages/Helpers/CommonMethods.cs
@@ -15,44 +15,44 @@ namespace MarsQA_1.Helpers
         //Screenshots
         //Screenshot
 
-        //public class SaveScreenShotClass
-        //{
-            
-        //    //public static string SaveScreenshot(IWebDriver driver, string ScreenShotFileName) // Definition
-        //    //{
-        //    //    var folderLocation = (ConstantHelpers.ScreenshotPath);
+        public class SaveScreenShotClass
+        {
 
-        //    //    if (!System.IO.Directory.Exists(folderLocation))
-        //    //    {
-        //    //        System.IO.Directory.CreateDirectory(folderLocation);
-        //    //    }
+            public static string SaveScreenshot(IWebDriver driver, string ScreenShotFileName) // Definition
+            {
+                var folderLocation = (ConstantHelpers.ScreenshotPath);
 
-        //    //    var screenShot = ((ITakesScreenshot)driver).GetScreenshot();
-        //    //    var fileName = new StringBuilder(folderLocation);
+                if (!System.IO.Directory.Exists(folderLocation))
+                {
+                    System.IO.Directory.CreateDirectory(folderLocation);
+                }
 
-        //    //    fileName.Append(ScreenShotFileName);
-        //    //    fileName.Append(DateTime.Now.ToString("_dd-mm-yyyy_mss"));
-        //    //    //fileName.Append(DateTime.Now.ToString("dd-mm-yyyym_ss"));
-        //    //    fileName.Append(".jpeg");
-        //    //    screenShot.SaveAsFile(fileName.ToString(), ScreenshotImageFormat.Jpeg);
-        //    //    return fileName.ToString();
-        //    //}
-        //}
+                var screenShot = ((ITakesScreenshot)driver).GetScreenshot();
+                var fileName = new StringBuilder(folderLocation);
 
-        ////ExtentReports
-        //#region reports
-        //public static ExtentTest test;
-        //public static ExtentReports Extent;
+                fileName.Append(ScreenShotFileName);
+                fileName.Append(DateTime.Now.ToString("_dd-mm-yyyy_mss"));
+                //fileName.Append(DateTime.Now.ToString("dd-mm-yyyym_ss"));
+                fileName.Append(".jpeg");
+                screenShot.SaveAsFile(fileName.ToString(), ScreenshotImageFormat.Jpeg);
+                return fileName.ToString();
+            }
+        }
+
+        //ExtentReports
+        #region reports
+        public static ExtentTest test;
+        public static ExtentReports Extent;
 
 
 
-        //public static void ExtentReports()
-        //{
-        //    Extent = new ExtentReports(ConstantHelpers.ReportsPath, true, DisplayOrder.NewestFirst);
-        //    Extent.LoadConfig(ConstantHelpers.ReportXMLPath);
-        //}
+        public static void ExtentReports()
+        {
+            Extent = new ExtentReports(ConstantHelpers.ReportsPath, true, DisplayOrder.NewestFirst);
+            Extent.LoadConfig(ConstantHelpers.ReportXMLPath);
+        }
     }
-   // #endregion
+    #endregion
 
 }
 
